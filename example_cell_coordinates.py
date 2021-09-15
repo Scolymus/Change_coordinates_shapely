9caf37e36abc32929e1e13f86d9208af89f8ccba
Scolymus <20840716+Scolymus@users.noreply.github.com>
2021-09-15 02:18:06 +0200

    Add files via upload

--- example_cell_coordinates.py
+++ example_cell_coordinates.py
@@ -8,19 +8,19 @@ from shapely.geometry.polygon import Polygon
 # Correct with your desired polygons. This is an example of use for a chessboard case.
 def draw_box(dst):
     all_boxes = []
-	cells_unit = []
+    cells_unit = []
     corners = [[0,0],[0,0],[0,0],[0,0]]
 
-	rows = ...	# Image size Y
-	cols = ...	# Image size X
-	boxes_rows = ... # Number of boxes in the Y direction
-	boxes_cols = ... # Number of boxes in the X direction
+    rows = ...	# Image size Y
+    cols = ...	# Image size X
+    boxes_rows = ... # Number of boxes in the Y direction
+    boxes_cols = ... # Number of boxes in the X direction
 
-	Hjump = ... # Size of box in Y direction
-	Wjump = ... # Size of box in X direction
+    Hjump = ... # Size of box in Y direction
+    Wjump = ... # Size of box in X direction
 
-	px0 = --- # DO from initial x coordinate
-	py0 = --- # DO from initial y coordinate
+    px0 = --- # DO from initial x coordinate
+    py0 = --- # DO from initial y coordinate
 
     for r in range(boxes_rows):
         W = Wjump
@@ -36,7 +36,7 @@ def draw_box(dst):
 
             px += W
 
-			# Correct maximum position of corners with image size
+            # Correct maximum position of corners with image size
             for c in corners:
                 if c[0] < 0:
                     c[0] = 0
@@ -49,18 +49,18 @@ def draw_box(dst):
                     c[1] = rows
 
 
-			# Paint the box in the image
+            # Paint the box in the image
             pts = np.array([corners[0],corners[1],corners[2],corners[3]], np.int32)
             pts = pts.reshape((-1,1,2))
             cv2.polylines(dst,[pts],True,(0,255,0))
 
-			# Create the box
+            # Create the box
             all_boxes.append(Polygon(corners))
 
-			# Paint box number
+            # Paint box number
             cv2.putText(dst,str(len(all_boxes)-1), (int(all_boxes[-1].centroid.coords[0][0]), int(all_boxes[-1].centroid.coords[0][1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (128,0,0), 1)
-
-			# Save also corners positions
+            
+            # Save also corners positions
             cells_unit.append(pts)
 
         py += H
@@ -71,31 +71,31 @@ def draw_box(dst):
 # particles positions are in pos_time[particle_id][property]
 # where property is: 0: time, 1: x, 2: y, 3: cell
 def trajectories_with_indexes():
-	for p in range(len(pos_time)):
-		for t in range(0, len(pos_time[p][0])):
-			points = [pos_time[p][1][t], pos_time[p][2][t]]
-			found = False
+    for p in range(len(pos_time)):
+        for t in range(0, len(pos_time[p][0])):
+            points = [pos_time[p][1][t], pos_time[p][2][t]]
+            found = False
 
-			# You can speed up this process if you look for neighbourhood cells instead of all of them.
-			# That would depend on how your cells are in the image.
-			boxes = cells_unit_shapely
-			boxes_num = range(0,len(all_boxes)-1)
+            # You can speed up this process if you look for neighbourhood cells instead of all of them.
+            # That would depend on how your cells are in the image.
+            boxes = cells_unit_shapely
+            boxes_num = range(0,len(all_boxes)-1)
 
 
-			for i in range(len(boxes)):
-				if Point(points).intersects(boxes[i]) == True:
-					g.pos_time[p][3][t] = boxes_num[i]
-					found = True
-					break
-			if found == False:
-            	g.pos_time[p][3][t] = -1
+            for i in range(len(boxes)):
+                if Point(points).intersects(boxes[i]) == True:
+                    g.pos_time[p][3][t] = boxes_num[i]
+                    found = True
+                    break
+            if found == False:
+                g.pos_time[p][3][t] = -1
 
 
 # Control boxes with keyboard.
 # I only include interaction to move the box to the left by pressing a
 # DO the same for other directions.
 def keyboard_control_boxes(dst, ms, show):
-	global ind_cell, cells_unit
+    global ind_cell, cells_unit
 
     # Init variables
     finish = True
@@ -113,10 +113,10 @@ def keyboard_control_boxes(dst, ms, show):
             return 0
         elif k == ord('a') or k == ord('A'):		#ASWD: Typical keys to move the position of the particle         
             if ind_cell > -1:
-				for i in range(len(cells_unit[ind_cell])):
-					cells_unit[ind_cell][i][0] += -1
-					cells_unit[ind_cell][i][0] += -1
-    	        cells_unit_shapely[ind_cell] = Polygon(cells_unit[ind_cell])
+                for i in range(len(cells_unit[ind_cell])):
+                    cells_unit[ind_cell][i][0] += -1
+                    cells_unit[ind_cell][i][0] += -1
+                cells_unit_shapely[ind_cell] = Polygon(cells_unit[ind_cell])
 
             dst = frame.copy()					
             draw_only_boxes_lines(dst)
